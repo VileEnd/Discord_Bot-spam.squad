402aede51883417bb3562fdec12df1741cbab747
Jan Kretschmer <jan.kretschmer@immowelt.de>
2020-12-14 12:29:14 +0100

    update files

--- app.js
+++ app.js
@@ -1,46 +1,9 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
-var _this = this;
 var Discord = require("discord.js");
 var request = require("request");
 var rcon = require("./rcon/app.js");
 var SourceQuery = require("sourcequery");
 var fs = require("fs");
-var configdir = "config/server1.json";
+var configdir = "config";
 var maxServers = 50;
 var players_dc = "";
 var maxplayers_dc = "";
@@ -56,10 +19,9 @@ if (!fs.existsSync(configdir)) {
 // Create config file if not exist
 fs.readdir(configdir, function (err, files) {
     try {
-        if (files.length < 1)
+        if (File.length < 1)
             var writeConfig = '{"debug":false,"token":"","apiSite":4,"apiUrl":"https://full uri here","serverIp":"","serverPort":"28015","enableRcon":"0","rconhost":"","rconport":"","rconpass":"","prefix":"!","roles":["Administrator","admins"],"queueMessage":"currently waiting in queue.","updateInterval":"3"}';
-        var jsonData;
-        jsonData = JSON.parse(writeConfig); //do not know why it doesnt accept it
+        var jsonData = JSON.parse(writeConfig); //do not know why it doesnt accept it
         fs.writeFile("config/server1.json", JSON.stringify(jsonData, null, 2), "utf8", function (err) {
             if (err) {
                 console.log("An error occured while writing JSON Object to File.");
@@ -71,7 +33,7 @@ fs.readdir(configdir, function (err, files) {
     catch (error) { }
 });
 fs.readdir(configdir, function (err, files) {
-    var _loop_1 = function () {
+    var _loop_1 = function (i) {
         if (i > maxServers) {
             console.log("Max servers is over " + maxServers);
             console.log("Please verify max servers and try again");
@@ -154,11 +116,15 @@ fs.readdir(configdir, function (err, files) {
         };
         // End Functions
         try {
-            config = require("./config/server" + i + ".json");
+            config = require("./config/server1.json");
         }
-        catch (error) { }
+        catch (error) {
+            console.error(error);
+        }
         var client = new Discord.Client();
-        var updateInterval = 1000 * config.updateInterval || 1000 * process.env.updateInterval || 1000 * 3;
+        var timevat = void 0;
+        timevat = 1000;
+        var updateInterval = timevat * config.updateInterval || 1000 * 3;
         var debug = process.env.debug || config.debug;
         var apiUrl = process.env.apiUrl || config.apiUrl;
         var apiSite = process.env.apiSite || config.apiSite;
@@ -177,112 +143,72 @@ fs.readdir(configdir, function (err, files) {
                 updateActivity();
             }, updateInterval);
         });
-        // function onMessageReaction(collected) {
-        //   const reaction = collected.first();
-        //   console.log("hi");
-        //   if (reaction.emoji.name === '🔄') {
-        //     refresh = true;
-        //     message.channel.send("refresh");
-        //   }
-        //onClientMessage(message) 
         var hello;
         client.on("message", function (message) {
-            if ((hello = message.content.toLowerCase()) === prefix + "ilovehawaiipizza") {
-                if (serverStatus) {
-                    console.log(serverStatus + " Is Offline");
-                    dc_message = new Discord.MessageEmbed()
-                        .setColor('#ffcc00')
-                        .setTitle('EU-Server-Status')
-                        .setThumbnail('https://thumbs.gfycat.com/GenuineHideousJuliabutterfly-size_restricted.gif')
-                        .addFields({ name: "Status", value: "Currently offline; maybe Updating see #announcements" })
-                        .setTimestamp()
-                        .setFooter('Created by: VileEnd', 'https://static-cdn.jtvnw.net/jtv_user_pictures/63d0d633-72a6-4fda-bfb0-709cea4dcf04-profile_image-70x70.png');
-                }
-                else {
-                    dc_message = new Discord.MessageEmbed()
-                        .setColor('#ffcc00')
-                        .setTitle('EU-Server-Status')
-                        .setThumbnail('https://media1.tenor.com/images/8f4550dcf8cef9f7fbf8306d303e50bf/tenor.gif')
-                        .addFields({ name: "Players: ", value: players_dc + "/" + maxplayers_dc }, { name: "Map:", value: "" + currentmap_dc }, { name: "Status", value: "" + dc_seedinState })
-                        .setTimestamp()
-                        .setFooter('Created by: VileEnd', 'https://static-cdn.jtvnw.net/jtv_user_pictures/63d0d633-72a6-4fda-bfb0-709cea4dcf04-profile_image-70x70.png');
-                }
-                message.channel
-                    .send(dc_message)
-                    .then(function (msgReact) {
-                    msgReact.react('🔄');
-                    msgReact.react('😍');
-                    var filter = function (reaction, user) {
-                        console.log("MOIN");
-                        setactivState = false;
-                        return ['🔄', '😍'].includes(reaction.emoji.name) && user.id === message.author.id;
-                    };
-                    msgReact.awaitReactions(filter, { max: 1, time: 60000 })
-                        .then(function (collected) {
-                        var reaction = collected.first();
-                        if (reaction.emoji.name === '🔄') {
-                            console.log("HEY!");
-                            if (!serverStatus) {
-                                var newdc_message = new Discord.MessageEmbed()
-                                    .setColor('#ffcc00')
-                                    .setTitle('EU-Server-Status')
-                                    .setThumbnail('https://media1.tenor.com/images/8f4550dcf8cef9f7fbf8306d303e50bf/tenor.gif')
-                                    .addFields({ name: "Players: ", value: players_dc + "/" + maxplayers_dc }, { name: "Map:", value: "" + currentmap_dc }, { name: "Status", value: "" + dc_seedinState })
-                                    .setTimestamp()
-                                    .setFooter('Created by: VileEnd', 'https://static-cdn.jtvnw.net/jtv_user_pictures/63d0d633-72a6-4fda-bfb0-709cea4dcf04-profile_image-70x70.png');
-                                msgReact.edit(newdc_message);
-                            }
-                        }
-                    });
-                });
+            if ((hello = message.content.toLowerCase()) !== prefix + "ilovehawaiipizza") {
+                return;
             }
-        });
-        //Current Map: ${currentmap_dc} ${EventAnnouncement_dc}`
-        if (enableRcon == 1) {
-            client.on("message", function (message) { return __awaiter(_this, void 0, void 0, function () {
-                var args, command, getMessage, argumentString;
-                return __generator(this, function (_a) {
-                    if (message.author.bot)
-                        return [2 /*return*/];
-                    if (message.content.indexOf(prefix) !== 0)
-                        return [2 /*return*/];
-                    args = message.content
-                        .slice(prefix.length)
-                        .trim()
-                        .split(/ +/g);
-                    command = args.shift().toLowerCase();
-                    if (command === "rcon") {
-                        // Checks for discord permission
-                        if (!message.member.roles.cache.some(function (r) { return roles.includes(r.name); }))
-                            return [2 /*return*/, message.reply("Sorry, you don't have permissions to use this!")];
-                        getMessage = args.join(" ");
-                        argumentString = "" + getMessage;
-                        // Rcon Config
-                        rconhost = process.env.rconhost || config.rconhost;
-                        rconport = process.env.rconport || config.rconport;
-                        rconpass = process.env.rconpass || config.rconpass;
-                        // Run rcon command.
-                        rcon.RconApp(argumentString, rconhost, rconport, rconpass, debug);
-                        // Send message back to discord that we are trying to relay the command.
-                        message.channel.send("Trying to relay command: " + getMessage);
+            if (serverStatus) {
+                console.log(serverStatus + " Is Offline");
+                dc_message = new Discord.MessageEmbed()
+                    .setColor('#ffcc00')
+                    .setTitle('EU-Server-Status')
+                    .setThumbnail('https://thumbs.gfycat.com/GenuineHideousJuliabutterfly-size_restricted.gif')
+                    .addFields({ name: "Status", value: "Currently offline; maybe Updating see #announcements" })
+                    .setTimestamp()
+                    .setFooter('Created by: VileEnd', 'https://static-cdn.jtvnw.net/jtv_user_pictures/63d0d633-72a6-4fda-bfb0-709cea4dcf04-profile_image-70x70.png');
+            }
+            else {
+                dc_message = new Discord.MessageEmbed()
+                    .setColor('#ffcc00')
+                    .setTitle('EU-Server-Status')
+                    .setThumbnail('https://media1.tenor.com/images/8f4550dcf8cef9f7fbf8306d303e50bf/tenor.gif')
+                    .addFields({ name: "Players: ", value: players_dc + "/" + maxplayers_dc }, { name: "Map:", value: "" + currentmap_dc }, { name: "Status", value: "" + dc_seedinState })
+                    .setTimestamp()
+                    .setFooter('Created by: VileEnd', 'https://static-cdn.jtvnw.net/jtv_user_pictures/63d0d633-72a6-4fda-bfb0-709cea4dcf04-profile_image-70x70.png');
+            }
+            message.channel
+                .send(dc_message)
+                .then(function (msgReact) {
+                msgReact.react('🔄');
+                msgReact.react('😍');
+                var filter = function (reaction, user) {
+                    console.log("MOIN");
+                    setactivState = false;
+                    return ['🔄', '😍'].includes(reaction.emoji.name) && user.id === message.author.id;
+                };
+                msgReact.awaitReactions(filter, { max: 1, time: 60000 })
+                    .then(function (collected) {
+                    var reaction = collected.first();
+                    if (reaction.emoji.name === '🔄') {
+                        console.log("HEY!");
+                        if (!serverStatus) {
+                            var newdc_message = new Discord.MessageEmbed()
+                                .setColor('#ffcc00')
+                                .setTitle('EU-Server-Status')
+                                .setThumbnail('https://media1.tenor.com/images/8f4550dcf8cef9f7fbf8306d303e50bf/tenor.gif')
+                                .addFields({ name: "Players: ", value: players_dc + "/" + maxplayers_dc }, { name: "Map:", value: "" + currentmap_dc }, { name: "Status", value: "" + dc_seedinState })
+                                .setTimestamp()
+                                .setFooter('Created by: VileEnd', 'https://static-cdn.jtvnw.net/jtv_user_pictures/63d0d633-72a6-4fda-bfb0-709cea4dcf04-profile_image-70x70.png');
+                            msgReact.edit(newdc_message);
+                        }
                     }
-                    return [2 /*return*/];
                 });
-            }); });
-        }
-        else if (debug)
-            console.log("Rcon mode disabled");
+            });
+        });
         client.on("guildCreate", function (guild) {
             console.log("New guild joined: " + guild.name + " (id: " + guild.id + "). This guild has " + guild.memberCount + " members!");
         });
         client.on("guildDelete", function (guild) {
             console.log("I have been removed from: " + guild.name + " (id: " + guild.id + ")");
         });
+        var error = void 0;
         client.on("error", function (error) {
             if (debug)
                 console.log(error);
         });
         process.on("unhandledRejection", function (error) {
+            console.log(typeof error);
             if (error.code == "TOKEN_INVALID")
                 return console.log("Error: An invalid token was provided.\nYou have maybe added client secret instead of BOT token.\nPlease set BOT token");
             return console.error("Unhandled promise rejection:", error);
@@ -291,6 +217,6 @@ fs.readdir(configdir, function (err, files) {
     };
     var config, setactivState;
     for (var i = 1; i <= files.length; i++) {
-        _loop_1();
+        _loop_1(i);
     }
 });
--- app.ts
+++ app.ts
@@ -3,7 +3,7 @@ const request = require("request");
 const rcon = require("./rcon/app.js");
 const SourceQuery = require("sourcequery");
 const fs = require("fs");
-const configdir:string = "config/server1.json";
+const configdir = "config";
 const maxServers = 50;
 let players_dc = "";
 let maxplayers_dc = "";
@@ -19,20 +19,19 @@ if (!fs.existsSync(configdir)) {
 }
 
 // Create config file if not exist
-fs.readdir(configdir, (err:any, files:any) => {
+fs.readdir(configdir, (err, files)=> {
   try {
-    if (files.length < 1)
+    if (File.length < 1)
       var writeConfig =
         '{"debug":false,"token":"","apiSite":4,"apiUrl":"https://full uri here","serverIp":"","serverPort":"28015","enableRcon":"0","rconhost":"","rconport":"","rconpass":"","prefix":"!","roles":["Administrator","admins"],"queueMessage":"currently waiting in queue.","updateInterval":"3"}';
 
-    var jsonData: any;
-    jsonData = JSON.parse(writeConfig);//do not know why it doesnt accept it
+   var  jsonData = JSON.parse(writeConfig);//do not know why it doesnt accept it
 
     fs.writeFile(
       "config/server1.json",
       JSON.stringify(jsonData, null, 2),
       "utf8",
-      function (err: any) {
+      function (err) {
         if (err) {
           console.log("An error occured while writing JSON Object to File.");
           return console.log(err);
@@ -43,7 +42,7 @@ fs.readdir(configdir, (err:any, files:any) => {
   } catch (error) { }
 });
 
-fs.readdir(configdir, (err: any, files: string | any[]) => {
+fs.readdir(configdir, (err, files) => {
   for (let i = 1; i <= files.length; i++) {
     if (i > maxServers) {
       console.log("Max servers is over " + maxServers);
@@ -62,7 +61,7 @@ fs.readdir(configdir, (err: any, files: string | any[]) => {
             headers: { json: true, Referer: "discord-rustserverstatus" },
             timeout: 10000
           },
-          function (err: any, res: { statusCode: number; }, body: string) {
+          function (err, res, body) {
             if (!err && res.statusCode == 200) {
               const jsonData = JSON.parse(body);
               const server = jsonData.data.attributes;
@@ -118,7 +117,7 @@ fs.readdir(configdir, (err: any, files: string | any[]) => {
           const sq = new SourceQuery(1000); // 1000ms timeout
           sq.open(serverIp, serverPort);
 
-          sq.getInfo(function (err: any, info: { players: any; maxplayers: any; }) {
+          sq.getInfo(function (err, info) {
             if (err) {
               return client.user.setActivity("Offline");
             } else {
@@ -135,12 +134,13 @@ fs.readdir(configdir, (err: any, files: string | any[]) => {
     // End Functions
 
     try {
-      var config = require("./config/server" + i + ".json");
-    } catch (error) { }
+      var config = require("./config/server1.json");
+    } catch (error) { console.error(error);}
     const client = new Discord.Client();
-
+    let timevat;
+    timevat = 1000;
     const updateInterval =
-        1000 * config.updateInterval || 1000 * process.env.updateInterval || 1000 * 3;
+        timevat * config.updateInterval || 1000 * 3;
     const debug = process.env.debug || config.debug;
     const apiUrl = process.env.apiUrl || config.apiUrl;
     const apiSite = process.env.apiSite || config.apiSite;
@@ -163,144 +163,91 @@ fs.readdir(configdir, (err: any, files: string | any[]) => {
       }, updateInterval);
     });
 
-
-
-
-
-
-    // function onMessageReaction(collected) {
-    //   const reaction = collected.first();
-    //   console.log("hi");
-    //   if (reaction.emoji.name === '🔄') {
-    //     refresh = true;
-    //     message.channel.send("refresh");
-    //   }
-    //onClientMessage(message) 
 let hello;
-    client.on("message", (message: { content: string; channel: { send: (arg0: string) => Promise<any>; }; author: { id: any; }; }) => {
-      if ((hello=message.content.toLowerCase()) === `${prefix}ilovehawaiipizza`) {
-
-        if (serverStatus) {
-          console.log(`${serverStatus} Is Offline`);
-          dc_message = new Discord.MessageEmbed()
+    client.on("message", message => {
+      if ((hello = message.content.toLowerCase()) !== `${prefix}ilovehawaiipizza`) {
+        return;
+      }
+      if (serverStatus) {
+        console.log(`${serverStatus} Is Offline`);
+        dc_message = new Discord.MessageEmbed()
             .setColor('#ffcc00')
             .setTitle('EU-Server-Status')
             .setThumbnail('https://thumbs.gfycat.com/GenuineHideousJuliabutterfly-size_restricted.gif')
             .addFields(
-              {name: `Status` ,value: "Currently offline; maybe Updating see #announcements"})
+                {name: `Status`, value: "Currently offline; maybe Updating see #announcements"})
             .setTimestamp()
             .setFooter('Created by: VileEnd', 'https://static-cdn.jtvnw.net/jtv_user_pictures/63d0d633-72a6-4fda-bfb0-709cea4dcf04-profile_image-70x70.png');
-        } else {
-          dc_message = new Discord.MessageEmbed()
+      } else {
+        dc_message = new Discord.MessageEmbed()
             .setColor('#ffcc00')
             .setTitle('EU-Server-Status')
             .setThumbnail('https://media1.tenor.com/images/8f4550dcf8cef9f7fbf8306d303e50bf/tenor.gif')
             .addFields(
-              { name: `Players: `, value: `${players_dc}/${maxplayers_dc}` },
-              { name: `Map:`, value: `${currentmap_dc}` },
-              { name: `Status`, value: `${dc_seedinState}` })
+                {name: `Players: `, value: `${players_dc}/${maxplayers_dc}`},
+                {name: `Map:`, value: `${currentmap_dc}`},
+                {name: `Status`, value: `${dc_seedinState}`})
             .setTimestamp()
             .setFooter('Created by: VileEnd', 'https://static-cdn.jtvnw.net/jtv_user_pictures/63d0d633-72a6-4fda-bfb0-709cea4dcf04-profile_image-70x70.png');
-        }
-
-        message.channel
+      }
+      message.channel
           .send(dc_message)
           .then(msgReact => {
             msgReact.react('🔄');
             msgReact.react('😍');
 
-            const filter = (reaction: { emoji: { name: string; }; }, user: { id: any; }) => {
+            const filter = (reaction, user) => {
               console.log("MOIN")
               setactivState = false
               return ['🔄', '😍'].includes(reaction.emoji.name) && user.id === message.author.id
             };
 
-            msgReact.awaitReactions(filter, { max: 1, time: 60000 })
-              .then((collected: { first: () => any; }) => {
-                const reaction = collected.first();
-                if (reaction.emoji.name === '🔄') {
-                  console.log("HEY!")
-if(!serverStatus){
-                  const newdc_message = new Discord.MessageEmbed()
-                  .setColor('#ffcc00')
-                  .setTitle('EU-Server-Status')
-                  .setThumbnail('https://media1.tenor.com/images/8f4550dcf8cef9f7fbf8306d303e50bf/tenor.gif')
-                  
-                  .addFields(
-                    { name: `Players: `, value: `${players_dc}/${maxplayers_dc}` },
-                    { name: `Map:`, value: `${currentmap_dc}` },
-                    { name: `Status`, value: `${dc_seedinState}` })
-                  
-                  .setTimestamp()
-                  .setFooter('Created by: VileEnd', 'https://static-cdn.jtvnw.net/jtv_user_pictures/63d0d633-72a6-4fda-bfb0-709cea4dcf04-profile_image-70x70.png')
-                  msgReact.edit(newdc_message);
-                }}
-              })
-          })
+            msgReact.awaitReactions(filter, {max: 1, time: 60000})
+                .then(collected => {
+                  const reaction = collected.first();
+                  if (reaction.emoji.name === '🔄') {
+                    console.log("HEY!")
+                    if (!serverStatus) {
+                      const newdc_message = new Discord.MessageEmbed()
+                          .setColor('#ffcc00')
+                          .setTitle('EU-Server-Status')
+                          .setThumbnail('https://media1.tenor.com/images/8f4550dcf8cef9f7fbf8306d303e50bf/tenor.gif')
 
-      }
+                          .addFields(
+                              {name: `Players: `, value: `${players_dc}/${maxplayers_dc}`},
+                              {name: `Map:`, value: `${currentmap_dc}`},
+                              {name: `Status`, value: `${dc_seedinState}`})
+
+                          .setTimestamp()
+                          .setFooter('Created by: VileEnd', 'https://static-cdn.jtvnw.net/jtv_user_pictures/63d0d633-72a6-4fda-bfb0-709cea4dcf04-profile_image-70x70.png')
+                      msgReact.edit(newdc_message);
+                    }
+                  }
+                })
+          })
     })
 
-    //Current Map: ${currentmap_dc} ${EventAnnouncement_dc}`
 
-
-   // if (enableRcon == 1) {
-   //    client.on("message", async (message: { author: { bot: any; }; content: string; member: { roles: { cache: { name: any; }[]; }; }; reply: (arg0: string) => any; channel: { send: (arg0: string) => void; }; }) => {
-   //      if (message.author.bot) return;
-   //      if (message.content.indexOf(prefix) !== 0) return;
-   //
-   //      var args = message.content
-   //        .slice(prefix.length)
-   //        .trim()
-   //        .split(/ +/g);
-   //      var command = args.shift().toLowerCase();
-   //
-   //      if (command === "rcon") {
-   //        // Checks for discord permission
-   //        if (!message.member.roles.cache.some((r: { name: any; }) => roles.includes(r.name)))
-   //          return message.reply(
-   //            "Sorry, you don't have permissions to use this!"
-   //          );
-   //
-   //        var getMessage = args.join(" ");
-   //
-   //        // Rcon message.
-   //        const argumentString = `${getMessage}`;
-   //
-   //        // Rcon Config
-   //        rconhost = process.env.rconhost || config.rconhost;
-   //        rconport = process.env.rconport || config.rconport;
-   //        rconpass = process.env.rconpass || config.rconpass;
-   //
-   //        // Run rcon command.
-   //        rcon.RconApp(argumentString, rconhost, rconport, rconpass, debug);
-   //
-   //        // Send message back to discord that we are trying to relay the command.
-   //        message.channel.send(`Trying to relay command: ${getMessage}`);
-   //      }
-   //    });
-   //  } else if (debug) console.log("Rcon mode disabled");
-
-    client.on("guildCreate", (guild: { name: any; id: any; memberCount: any; }) => {
+    client.on("guildCreate", (guild) => {
       console.log(
         `New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`
       );
     });
 
-    client.on("guildDelete", (guild: { name: any; id: any; }) => {
+    client.on("guildDelete", (guild) => {
       console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
     });
-
-    client.on("error", function (error: any) {
+    let error;
+ client.on("error", error =>{
       if (debug) console.log(error);
     });
 
     process.on("unhandledRejection", (error:any) => {
+      console.log(typeof error)
       if (error.code == "TOKEN_INVALID")
         return console.log(
           "Error: An invalid token was provided.\nYou have maybe added client secret instead of BOT token.\nPlease set BOT token"
         );
-
       return console.error("Unhandled promise rejection:", error);
     });
 
